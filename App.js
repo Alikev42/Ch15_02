eb75adb94683ea0915633d79b0b0a5ced7519dc3
Kevin R Salger <kevin.salger@student.csulb.edu>
2021-05-30 18:43:52 -0700

    Ch15pg279

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+//  Kevin R. Salger  //
+//  IS657, Ch 15 pg 279 practice  //
+
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
@@ -5,17 +8,49 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.box}>
+        <Text style={styles.boxText}>#1</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.boxText}>#2</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.boxText}>#3</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container: 
+  {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: "column",
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
+    backgroundColor: 'ghostwhite',
+    Platform.select({
+      ios: {paddingTop: 20},
+      android: {paddingTop: StatusBar.currentHeight}
+    })
   },
+
+  box:
+  {
+    width: 300,
+    height: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "lightgray",
+    borderWidth: 1,
+    borderStyle: "dashed",
+    borderColor:
+  },
+
+  boxText
+  {
+    color: "darkslategray",
+    fontWeight: "bold"
+  }
 });
